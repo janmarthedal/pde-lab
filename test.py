5ac69ef47116490134eebb138876a97cd1eb2072
Jan Marthedal Rasmussen <jan@janmr.com>
2025-06-17 13:29:58 +0200

    Support for non-constant basis grads

--- test.py
+++ test.py
@@ -1,91 +1,103 @@
-from collections.abc import Callable
 import numpy as np
 from meshio import Mesh
 from meshgen import rect2d
 from scipy.integrate import dblquad
-from scipy.linalg import det, inv, solve
+from scipy.linalg import det, inv, svd
 from scipy.sparse import coo_array, csr_array
 
 
-def get_triangle_fun(i: int) -> Callable[[np.array], np.array]:
-    match i:
-        case 0: return lambda p: 1 - p[0] - p[1]
-        case 1: return lambda p: p[0]
-        case 2: return lambda p: p[1]
-    assert False
+# def get_triangle_fun(i: int) -> Callable[[np.array], np.array]:
+#     match i:
+#         case 0: return lambda p: 1 - p[0] - p[1]
+#         case 1: return lambda p: p[0]
+#         case 2: return lambda p: p[1]
+#     assert False
 
 
-def get_triangle_grad_fun(i: int) -> Callable[[np.array], np.array]:
-    match i:
-        case 0: return lambda _: np.array([-1., -1.])
-        case 1: return lambda _: np.array([1., 0.])
-        case 2: return lambda _: np.array([0., 1.])
-    assert False
+# def get_triangle_grad_fun(i: int) -> Callable[[np.array], np.array]:
+#     match i:
+#         case 0: return lambda _: np.array([-1., -1.])
+#         case 1: return lambda _: np.array([1., 0.])
+#         case 2: return lambda _: np.array([0., 1.])
+#     assert False
 
 
-def triangle_integrate_2d(points: np.array, i: int, j: int) -> float:
-    assert points.shape == (3, 2)
-
-    # mapping local coordinates R^2 -> R^2, s=0..1, t=0..1-s
-
-    # The Jacobian matrix
-    J = points[1:3] - points[0]
-    # The Jacobian
-    jac = np.abs(det(J))
-
-    f1 = get_triangle_fun(i)
-    f2 = get_triangle_fun(j)
-    f = lambda s, t: f1([s, t]) * f2([s, t])
-    v, _ = dblquad(f, 0, 1, 0, lambda s: 1 - s)
-
-    return jac * v
+def get_triangle_grad_fun():
+    # returns
+    #   dN1/ds, dN1/dt
+    #   dN2/ds, dN2/dt
+    #   dN3/ds, dN3/dt
+    return lambda _: np.array([
+        [-1., -1.],
+        [ 1.,  0.],
+        [ 0.,  1.]
+    ])
 
 
-def triangle_integrate_grad_2d(points: np.array, i: int, j: int) -> float:
-    assert points.shape == (3, 2)
+def triangle_element_grad_var(element_points, basis_grad, i: int, j: int) -> float:
+    def integrand(s, t):
+        bg = basis_grad(np.array([s, t]))
+        J = bg.T @ element_points
+        Jinv = inv(J)
+        jac = np.abs(det(J))
+        return jac * np.dot(Jinv @ bg[i], Jinv @ bg[j])
+    v, _ = dblquad(integrand, 0, 1, 0, lambda s: 1 - s)
+    return v
 
-    # mapping local coordinates R^2 -> R^2, s=0..1, t=0..1-s
 
-    # The Jacobian matrix
-    J = points[1:3] - points[0]
+def triangle_element_grad_const(J: np.array, grad_i: np.array, grad_j: np.array) -> float:
     Jinv = inv(J)
-    # The Jacobian
-    jac = np.abs(det(J))
-
-    f1 = get_triangle_grad_fun(i)
-    f2 = get_triangle_grad_fun(j)
-    f = lambda s, t: np.dot(np.dot(Jinv, f1([s, t])), np.dot(Jinv, f2([s, t])))
-    v, _ = dblquad(f, 0, 1, 0, lambda s: 1 - s)
-
-    return jac * v
+    # 0.5 is the integral of 1 with s=0..1, t=0..1-s (area of half a square)
+    return 0.5 * np.abs(det(J)) * np.dot(Jinv @ grad_i, Jinv @ grad_j)
 
 
-def assemble(mesh: Mesh) -> csr_array:
-    assert mesh.points.shape[1] == 2
-    point_count = mesh.points.shape[0]
-    print(f"point_count {point_count}")
+def triangle_grad_dot_grad(points: np.array, elements: np.array) -> coo_array:
+    order = elements.shape[1]
+    basis_grad = get_triangle_grad_fun()
 
     # pre-allocate np arrays instead?
     row: list[float] = []
     col: list[float] = []
     data: list[float] = []
 
+    for element_idx in elements:
+        element_points = points[element_idx, :]
+
+        if callable(basis_grad):
+            J = None
+        else:
+            J = np.dot(basis_grad.T, element_points)
+
+        for i in range(0, order):
+            for j in range(0, order):
+                # exploit symmetry?
+                if J is None:
+                    v = triangle_element_grad_var(element_points, basis_grad, i, j)
+                else:
+                    v = triangle_element_grad_const(J, basis_grad[i], basis_grad[j])
+                row.append(element_idx[i])
+                col.append(element_idx[j])
+                data.append(v)
+
+    return coo_array(
+        (data, (row, col)),
+        shape=(points.shape[0], points.shape[0]),
+        dtype=np.float64
+    ).tocsr()
+
+
+def assemble(mesh: Mesh) -> csr_array:
+    points = mesh.points
+    dim = points.shape[0]
+    A = csr_array((dim, dim), dtype=np.float64)
+
     for cell_block in mesh.cells:
         if cell_block.type == 'triangle':
-            for element_idx in cell_block.data:
-                element_points = mesh.points[element_idx, :]
-                for i in range(0, 3):
-                    for j in range(0, 3):
-                        # exploit symmetry?
-                        v = triangle_integrate_grad_2d(element_points, i, j)
-                        row.append(element_idx[i])
-                        col.append(element_idx[j])
-                        data.append(v)
+            A += triangle_grad_dot_grad(points, cell_block.data)
         else:
             print(f"Unsupported cell block {cell_block}")
 
-    A = coo_array((data, (row, col)), shape=(point_count, point_count), dtype=np.float64)
-    return A.tocsr()
+    return A
 
 
 mesh = rect2d(5, 4, 5, 3)
